8171b3fa2b67a7f85e19ad71c56e717a07de13c3
boypansit <boypansit1995@gmail.com>
2015-04-25 21:40:51 +0700

    *05* add score

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -25,6 +25,7 @@ public class GamePanel extends JPanel {
 		
 		big.setColor(Color.white);		
 		big.drawString(String.format("%08d", reporter.getScore()), 300, 20);
+		big.drawString(String.format("%08d", reporter.getScore()), 300, 40);
 		for(Sprite s : sprites){
 			s.draw(big);
 		}
